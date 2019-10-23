f3620da18ea7a6b2b742beb6b16b4d1e50eecfef
Jimil Patel <jimil.patel@jimilpatel-ltm1.internal.salesforce.com>
2019-10-23 15:05:00 -0700

    untracked node_modules

--- server.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var violetSrvr = require('violet/lib/violetSrvr')('/violet');
-violetSrvr.listAppsAt('/');
-var srvrInstance = violetSrvr.createAndListen(process.env.PORT || 8080);
-violetSrvr.loadScript('gameNight.js', 'trailhead');
-console.log('Waiting for requests...');
